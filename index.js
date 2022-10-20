12e446097b7c38367fb3f38a16d44c14859f46b5
Dahlia <rlally89@gmail.com>
2022-10-20 11:09:09 -0700

    linked packages, set up prompts

---
+++ index.js
@@ -0,0 +1,45 @@
+//packages needed for HTLM application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const html = require("./template")
+
+//questions to prompt the user
+ inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'Where are you located?',
+        },
+        {
+            type: 'input',
+            name: 'bio',
+            message: 'Include your bio:',
+        },
+        {
+            type: 'input',
+            name: 'linkedin',
+            message: 'What is your LinkedIn URL?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub URL?',
+        },
+    ])
+    .then((response) => {
+        console.log(response)
+        fs.writeFile(`${response.name}.html`, JSON.stringify(response,null,4),errorhandle)
+      });
+    
+      const errorhandle = (err) => {
+        if (err){
+            throw err;
+        }
+        console.log("Generating your portfolio page.")
+      }
